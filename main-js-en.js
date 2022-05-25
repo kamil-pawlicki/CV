5f5e39e49044992566217af83b6f6216843db71e
Kamil Pawlicki <k.l.pawlicki@gmail.com>
2022-05-25 14:23:35 +0200

    Tips in console, all JS in both language version

--- main-js-en.js
+++ main-js-en.js
@@ -1,7 +1,40 @@
 console.log(`Hello Dear Visitor :)`);
-console.log(`This site was create to make a little presentation knowledge of code elements. The website is not graphically consistent and not perfect, but it is intended to show that I understand the code and know how to use it. The website is interactive and have different elements of CSS and J-S. Here are some tips:`);
-console.log(`This website is create in both languages Polish and English. You are able to change language in navigation in top right corner. For each language is different JS file !!!`);
-console.log(`Let's see what happens when you click on some article`);
-console.log(`If my contact details is too small to read them, just hover over it with mouse cursor - then it should go bigger to read`);
-console.log(`Links in section Contacts are interactiv as well`);
-console.log(`When screen size is change, the website will adapt and change parameters`);
+console.log(`This site was create to make a little presentation of knowledge code elements. 
+The website is not graphically consistent and not perfect, 
+but it is intended to show that I understand the code and know how to use it. 
+The website is INTERACTIVE and have differents elements of CSS and J-S. 
+    Here are some TIPS:`);
+console.log(
+`1) This website is create in both languages Polish and English. You are able to change language in navigation in top right corner. For each language is different JS file !!!`
+);
+
+console.log(`2) Let's see what happens when you click on some article 
+        --> When F12 dev tolls is closed then just slide mouse cousor`);
+
+console.log(
+`3) If my contact details is too small to read them, just click on it using mouse - then it goes bigger to read`
+);
+
+console.log(`4) Links in section Contacts are interactiv as well`);
+
+console.log(`5) When screen size is change, the website will adapt and change parameters`);
+
+{
+  const button = document.querySelector(".mail--js");
+
+  console.log(button);
+
+  const myClick = () => {
+    const button = document.querySelector(".mail--js");
+    button.innerHTML = "<h2>k.l.pawlicki@gmail.com</h2>";
+  };
+  button.addEventListener("click", myClick);
+}
+{
+  const button = document.querySelector(".phone--js");
+  const myNextClick = () => {
+    const button = document.querySelector(".phone--js");
+    button.innerHTML = "<h2>tel : (+48) 782-188-757</h2>";
+  };
+  button.addEventListener("click", myNextClick);
+}
--- main-js-pl.js
+++ main-js-pl.js
@@ -1,9 +1,36 @@
-console.log(`Witaj drogi odbiorco :)`);
-console.log(`Ta strona została stworzona z myślą o zaprezentowaniu znajomości poszczególnych elementów kodowania. 
-Nie jest dopracowana graficznie ani dopieszczona, ma na celu pokazać że rozumiem kod, polecenia i 
-umiem je wykorzystać. Strona jest interaktywna i posiada różne elementy CSS i J-S. Oto kilka wskazówek:`);
-console.log(`Ta strona jest dostępna w dwóch językach Polskim oraz Angielskim. Zmienić język możesz w nawigacji w górnym prawym rogu. Do każdego języka jest osobny plik JS !!!`);
-console.log(`sprawdźmy co się stanie gdy klikniesz w jakiś artykuł`);
-console.log(`Jeśli moje dane kontaktowe są za małe do przeczytania, to najedź na nie kursorem myszki - powinny się wtedy powiększyć`);
-console.log(`Linki w sekcji Kontakty również są interaktywne`);
-console.log(`Kiedy zmienisz rozdzielczość ekranu strona będzię się dostosowywać zmieniając parametry`);
+console.log(`Witaj drogi odbiorco :)`
+);
+console.log(`Ta strona została stworzona z myślą o zaprezentowaniu znajomości poszczególnych 
+elementów kodowania. Nie jest dopracowana graficznie ani dopieszczona, ma na celu 
+pokazać, że rozumiem kod, polecenia i umiem je wykorzystać. Strona jest INTERAKTYWNA 
+ i posiada różne elementy CSS i J-S. 
+ Oto kilka wskazówek:`);
+console.log(`1) Ta strona jest dostępna w dwóch językach Polskim oraz Angielskim. 
+ Zmienić język możesz w nawigacji w górnym prawym rogu. Do każdego języka jest osobny plik JS !!!`
+);
+console.log(`2) Sprawdźmy co się stanie gdy klikniesz w jakiś artykuł 
+        --> Gdy narzędzie deweloperskie F12 jest wyłączone, wtedy poprostu najedź kursorem myszki`);
+console.log(`3) Jeśli moje dane kontaktowe są za małe do przeczytania, to kliknij w nie by powiększyć`
+);
+console.log(`4) Linki w sekcji Kontakty również są interaktywne`);
+console.log(`5) Kiedy zmienisz rozdzielczość ekranu strona będzię się dostosowywać zmieniając parametry`
+);
+{
+  const button = document.querySelector(".mail--js");
+
+  console.log(button);
+
+  const myClick = () => {
+    const button = document.querySelector(".mail--js");
+    button.innerHTML = "<h2>k.l.pawlicki@gmail.com</h2>";
+  };
+  button.addEventListener("click", myClick);
+}
+{
+  const button = document.querySelector(".phone--js");
+  const myNextClick = () => {
+    const button = document.querySelector(".phone--js");
+    button.innerHTML = "<h2>tel : (+48) 782-188-757</h2>";
+  };
+  button.addEventListener("click", myNextClick);
+}
